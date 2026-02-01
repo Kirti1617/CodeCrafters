d2a9152c8d58d4aa780bf83c8bd3c8fbdcadf77d
Raghav Kumar Jha <raghav2002jha@gmail.com>
2026-02-01 14:58:01 +0530

    Initial project setup from local system

---
+++ resources/pages/administrator/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="resources/images/logo/attnlg.png" rel="icon">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="resources/assets/css/styles.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.2.0/remixicon.css" rel="stylesheet">
+</head>
+
+<body>
+
+</body>
+
+</html>
