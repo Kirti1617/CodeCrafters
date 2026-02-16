a580c06ecd9493925d44be26b82a71bc9afb7028
Raghav Kumar Jha <raghav2002jha@gmail.com>
2026-02-16 10:40:30 +0530

    Updated manage-course.php

--- resources/pages/administrator/manage-course.php
+++ resources/pages/administrator/manage-course.php
@@ -87,4 +87,39 @@ if (isset($_POST["addFaculty"])) {
 }
 
 
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="resources/images/logo/attnlg.png" rel="icon">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="resources/assets/css/admin_styles.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/4.2.0/remixicon.css" rel="stylesheet">
+</head>
+
+<body>
+    <?php include 'includes/topbar.php' ?>
+    <section class="main">
+        <?php include 'includes/sidebar.php'; ?>
+        <div class="main--content">
+            <div id="overlay"></div>
+            <div class="overview">
+                <div class="title">
+                    <h2 class="section--title">Overview</h2>
+                    <select name="date" id="date" class="dropdown">
+                        <option value="today">Today</option>
+                        <option value="lastweek">Last Week</option>
+                        <option value="lastmonth">Last Month</option>
+                        <option value="lastyear">Last Year</option>
+                        <option value="alltime">All Time</option>
+                    </select>
+                </div>
+
+</body>
+
+</html>
