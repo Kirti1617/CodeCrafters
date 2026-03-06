f4a52038045723582399b6e19900223b91947485
Raghav Kumar Jha <raghav2002jha@gmail.com>
2026-03-06 19:38:04 +0530

    Added home.php in lecture folder

---
+++ resources/pages/Lecture/home.php
@@ -0,0 +1,18 @@
+<?php
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $attendanceData = json_decode(file_get_contents("php://input"), true);
+    if ($attendanceData) {
+        try {
+            $sql = "INSERT INTO tblattendance (studentRegistrationNumber, course, unit, attendanceStatus, dateMarked)  
+                VALUES (:studentID, :course, :unit, :attendanceStatus, :date)";
+
+            $stmt = $pdo->prepare($sql);
+
+            foreach ($attendanceData as $data) {
+                $studentID = $data['studentID'];
+                $attendanceStatus = $data['attendanceStatus'];
+                $course = $data['course'];
+                $unit = $data['unit'];
+                $date = date("Y-m-d");
