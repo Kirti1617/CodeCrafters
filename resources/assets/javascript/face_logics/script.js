872143fd543e5b3c7e9dad3ff5d942446b8021fd
Raghav Kumar Jha <raghav2002jha@gmail.com>
2026-03-13 21:52:31 +0530

    Added script.js file in face_logics folder

---
+++ resources/assets/javascript/face_logics/script.js
@@ -0,0 +1,28 @@
+var labels = [];
+let detectedFaces = [];
+let sendingData = false;
+
+function updateTable() {
+  var selectedCourseID = document.getElementById("courseSelect").value;
+  var selectedUnitCode = document.getElementById("unitSelect").value;
+  var selectedVenue = document.getElementById("venueSelect").value;
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", "resources/pages/lecture/manageFolder.php", true);
+  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var response = JSON.parse(xhr.responseText);
+      if (response.status === "success") {
+        labels = response.data;
+
+        if (selectedCourseID && selectedUnitCode && selectedVenue) {
+          updateOtherElements();
+        }
+        document.getElementById("studentTableContainer").innerHTML =
+          response.html;
+      } else {
+        console.error("Error:", response.message);
+      }
+    }
+  };
